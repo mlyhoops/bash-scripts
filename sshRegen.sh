7eb4f56c4efef2f8ecf8d4d305624b751bc6d713
Matthew Yamamoto <mlyhoops@gmail.com>
2023-10-23 03:24:49 -0700

    Create sshRegen.sh

---
+++ sshRegen.sh
@@ -0,0 +1,2 @@
+# This script will reconfigure the SSH keys. This is needed because the keys are different for every machine and when cloning a new VM in Proxmox, you need to create new SSH keys so the machine doesn't get confused.
+sudo dpkg-reconfigure openssh-server
