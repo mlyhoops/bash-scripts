bbe2bab137de2c1cf433f8157bad9db31675c36d
Matthew Yamamoto <mlyhoops@gmail.com>
2023-10-23 03:26:37 -0700

    Update sshRegen.sh

--- sshRegen.sh
+++ sshRegen.sh
@@ -1,2 +1,4 @@
+#Debian or Ubuntu Linux Regenerate OpenSSH Host Keys
+
 # This script will reconfigure the SSH keys. This is needed because the keys are different for every machine and when cloning a new VM in Proxmox, you need to create new SSH keys so the machine doesn't get confused.
 sudo dpkg-reconfigure openssh-server
